a28cc8ea2667c1a77db08af7a3ea390d30d7872d
Ken <123234470+KenADM@users.noreply.github.com>
2026-05-15 14:53:55 +0200

    Implemented trigger 4 + testing

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE COLLEGAMENTO (
     OraArrivo TIME,
     NomeComp VARCHAR(50),
     CodiceRegistrazione CHAR(10),
-    PRIMARY KEY (Num, Codice, NomePartenza, NomeArrivo, NomeComp, CodiceRegistrazione),
+    PRIMARY KEY (Num, Codice, NomePartenza, NomeArrivo, CodiceRegistrazione),
     FOREIGN KEY (NomePartenza) REFERENCES CITTA(Nome) ON UPDATE CASCADE,
     FOREIGN KEY (NomeArrivo) REFERENCES CITTA(Nome) ON UPDATE CASCADE,
     FOREIGN KEY (NomeComp) REFERENCES COMPAGNIA(Nome) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -47,3 +47,90 @@ CREATE TABLE PROPRIETA (
 );
 
 
+--TRIGGER--
+
+-- 1 orario dei collegamenti, l'arrivo deve essere > della partenza
+CREATE OR REPLACE FUNCTION controlla_orario_collegamento()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$
+BEGIN
+    -- Controlliamo se l'orario di arrivo è minore o uguale a quello di partenza
+    IF NEW.OraArrivo <= NEW.OraPartenza THEN
+        -- Blocchiamo l'operazione con un messaggio di errore chiaro
+        RAISE EXCEPTION 'Errore orario: L''ora di arrivo (%) deve essere successiva all''ora di partenza (%) per il collegamento numero %.', NEW.OraArrivo, NEW.OraPartenza, NEW.Num;
+    END IF;
+
+    -- Se l'orario è corretto (Arrivo > Partenza), lasciamo passare il dato
+    RETURN NEW;
+END;
+$$ ;
+
+CREATE TRIGGER check_orari_collegamento
+BEFORE INSERT OR UPDATE ON Collegamento
+FOR EACH ROW
+EXECUTE FUNCTION controlla_orario_collegamento();
+
+-- ridondanza NumCompagnieColleganti
+
+-- ridondanza NumCittaServite
+
+-- 2 COLLEGAMENTO deve usare una barca che appartiene alla compagnia che offre il COLLEGAMENTO
+
+
+-- 3 impedire che due compagnie diverse abbiano acquistato la stessa barca nello stesso momento
+CREATE OR REPLACE FUNCTION controlla_data_acquisto_barca()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$
+BEGIN
+    -- Cerchiamo SE ESISTE GIÀ una riga nel database che va in conflitto con NEW
+    IF EXISTS (
+        SELECT * FROM Proprieta P
+        WHERE P.CodiceRegistrazione = NEW.CodiceRegistrazione -- Stessa barca
+        AND P.DataInizio = NEW.DataInizio                     -- Stesso giorno
+        AND P.NomeComp != NEW.NomeComp                        -- MA compagnia diversa!
+    ) THEN
+        -- Se trova un conflitto, blocca tutto
+        RAISE EXCEPTION 'Errore: La barca % è già stata acquistata da un''altra compagnia in data %.', NEW.CodiceRegistrazione, NEW.DataInizio;
+    END IF;
+
+    -- Se non ci sono conflitti, diamo il via libera
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER check_data_acquisto_barca
+BEFORE INSERT OR UPDATE ON Proprieta
+FOR EACH ROW
+EXECUTE FUNCTION controlla_data_acquisto_barca();
+
+-- 4 stessa barca utilizzata in tratte contemporanee
+CREATE OR REPLACE FUNCTION controlla_barche_contemporanee()
+RETURNS TRIGGER LANGUAGE plpgsql AS $$
+BEGIN
+    
+    IF EXISTS (
+        SELECT 1 FROM Collegamento C
+        WHERE C.CodiceRegistrazione = NEW.CodiceRegistrazione -- Stessa barca
+        AND (
+            C.Codice <> NEW.Codice
+            OR C.Num <> NEW.Num
+            OR C.NomePartenza <> NEW.NomePartenza
+            OR C.NomeArrivo <> NEW.NomeArrivo
+            OR C.CodiceRegistrazione <> NEW.CodiceRegistrazione
+            ) -- Escludiamo il collegamento stesso (utile per UPDATE)
+        AND C.OraPartenza < NEW.OraArrivo 
+        AND C.OraArrivo > NEW.OraPartenza
+    ) THEN
+        -- Se trova un conflitto, blocca tutto
+        RAISE EXCEPTION 'Errore: La barca % è già assegnata a collegamento esistente.', NEW.CodiceRegistrazione;
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER check_barche_contemporanee
+BEFORE INSERT OR UPDATE ON Collegamento
+FOR EACH ROW
+EXECUTE FUNCTION controlla_barche_contemporanee();
+
+
+-- trigger relativi alle chiavi esterne
--- sql/04_trigger_test.sql
+++ sql/04_trigger_test.sql
@@ -5,7 +5,7 @@ DECLARE
     test_superato BOOLEAN := TRUE;
 BEGIN
 
-    -- 1. SETUP DATI DI BASE
+    -- SETUP DATI DI BASE
     INSERT INTO COMPAGNIA VALUES ('CompagniaProva1', 'Mario Rossi', 999.00, 0);
     INSERT INTO IMBARCAZIONE VALUES ('NAV-1', 2001, 24000, 'T');
     INSERT INTO CITTA VALUES ('A', 'AA', 'NA', 960000, 0), ('B', 'BB', 'PA', 630000, 0);
@@ -14,7 +14,7 @@ BEGIN
     -- TEST POSITIVO: Arrivo > Partenza
     BEGIN
         INSERT INTO COLLEGAMENTO (Num, Codice, NomePartenza, OraPartenza, NomeArrivo, OraArrivo, NomeComp, CodiceRegistrazione) 
-        VALUES (1, '13', 'A', '1900-01-01 16:30:00', 'B', '1900-01-01 21:45:00', 'CompagniaProva1', 'NAV-1');
+        VALUES (1, '13', 'A', '16:30:00', 'B', '21:45:00', 'CompagniaProva1', 'NAV-1');
         RAISE NOTICE 'Trigger 1 - Test Positivo: OK';
     EXCEPTION WHEN OTHERS THEN
         RAISE NOTICE 'Trigger 1 - Test Positivo FALLITO: %', SQLERRM;
@@ -24,13 +24,20 @@ BEGIN
     -- TEST NEGATIVO: Arrivo < Partenza (Deve dare errore)
     BEGIN
         INSERT INTO COLLEGAMENTO (Num, Codice, NomePartenza, OraPartenza, NomeArrivo, OraArrivo, NomeComp, CodiceRegistrazione) 
-        VALUES (2, '14', 'A', '1900-01-01 16:30:00', 'B', '1900-01-01 15:00:00', 'CompagniaProva1', 'NAV-1');
+        VALUES (2, '14', 'A', '16:30:00', 'B', '15:00:00', 'CompagniaProva1', 'NAV-1');
         
         RAISE NOTICE 'Trigger 1 - Test Negativo FALLITO (Il trigger non ha bloccato)';
         test_superato := FALSE;
     EXCEPTION WHEN OTHERS THEN
         RAISE NOTICE 'Trigger 1 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
     END;
+
+    -- PULIZIA DATI
+    DELETE FROM COLLEGAMENTO WHERE Num = 1 AND Codice = '13' AND NomePartenza = 'A' AND NomeArrivo = 'B' AND CodiceRegistrazione = 'NAV-1';
+    DELETE FROM PROPRIETA WHERE NomeComp = 'CompagniaProva1' AND CodiceRegistrazione = 'NAV-1' AND DataInizio = '2004-03-23';
+    DELETE FROM CITTA WHERE Nome IN ('A', 'B');
+    DELETE FROM IMBARCAZIONE WHERE CodiceRegistrazione = 'NAV-1';
+    DELETE FROM COMPAGNIA WHERE Nome = 'CompagniaProva1';
 
     RETURN test_superato;
 END; $$ LANGUAGE plpgsql;
@@ -44,7 +51,7 @@ DECLARE
     test_superato BOOLEAN := TRUE;
 BEGIN
 
-    -- 1. SETUP
+    -- SETUP
     INSERT INTO COMPAGNIA VALUES ('CompagniaProva2', 'Mario Rossi', 999.00, 0), ('CompagniaProva3', 'Mario Rossi', 999.00, 0);
     INSERT INTO IMBARCAZIONE VALUES ('NAV-3', 2001, 24000, 'T');
 
@@ -67,10 +74,83 @@ BEGIN
         RAISE NOTICE 'Trigger 3 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
     END;
 
+    -- PULIZIA DATI
+    DELETE FROM PROPRIETA WHERE NomeComp = 'CompagniaProva2' AND CodiceRegistrazione = 'NAV-3' AND DataInizio = '2004-03-23';
+    DELETE FROM IMBARCAZIONE WHERE CodiceRegistrazione = 'NAV-3';
+    DELETE FROM COMPAGNIA WHERE Nome IN ('CompagniaProva2', 'CompagniaProva3');
+
     RETURN test_superato;
 END; $$ LANGUAGE plpgsql;
 
+--- Test per il Trigger 4: Verifica che la stessa barca non possa essere utilizzata in collegamenti contemporanei.
+CREATE OR REPLACE FUNCTION test_trigger_4_collegamenti()
+RETURNS BOOLEAN AS $$
+DECLARE
+    test_superato BOOLEAN := TRUE;
+BEGIN
 
--- Aggiungi queste righe in fondo al file 04_trigger_test.sql
+    -- SETUP
+    INSERT INTO COMPAGNIA VALUES ('CompagniaProva', 'Mario Rossi', 999.00, 0);
+    INSERT INTO IMBARCAZIONE VALUES ('NAV-3', 2001, 24000, 'T');
+    INSERT INTO PROPRIETA VALUES ('CompagniaProva', 'NAV-3', '2004-03-23');
+    INSERT INTO CITTA VALUES ('A', 'AA', 'NA', 960000, 0), ('B', 'BB', 'PA', 630000, 0);
+
+    -- TEST POSITIVO: Prima assegnazione
+    BEGIN
+        INSERT INTO COLLEGAMENTO VALUES (1, '15', 'A', '16:30:00', 'B', '21:45:00', 'CompagniaProva', 'NAV-3');
+        RAISE NOTICE 'Trigger 4 - Test Positivo: OK';
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Trigger 4 - Test Positivo FALLITO: %', SQLERRM;
+        test_superato := FALSE;
+    END;
+
+    -- TEST NEGATIVO: Seconda assegnazione (Se vietata, deve dare errore)
+
+    -- TEST UNO: nuovo collegamento inizia mentre la barca è utilizzata
+    BEGIN
+        INSERT INTO COLLEGAMENTO VALUES (2, '16', 'A', '17:00:00', 'B', '22:00:00', 'CompagniaProva', 'NAV-3');
+        RAISE NOTICE 'Trigger 4 - Test Negativo FALLITO (Il trigger non ha bloccato)';
+        test_superato := FALSE;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Trigger 4 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
+    END;
+    -- TEST DUE: nuovo collegamento usa la barca, ma in mezzo è utilizzata da un altro collegamento
+    BEGIN
+        INSERT INTO COLLEGAMENTO VALUES (3, '17', 'A', '15:00:00', 'B', '23:00:00', 'CompagniaProva', 'NAV-3');
+        RAISE NOTICE 'Trigger 4 - Test Negativo FALLITO (Il trigger non ha bloccato)';
+        test_superato := FALSE;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Trigger 4 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
+    END;
+    -- TEST TRE: nuovo collegamento usa barca ,ma prima di tornare è utilizzata da un altro collegamento
+    BEGIN
+        INSERT INTO COLLEGAMENTO VALUES (4, '18', 'A', '16:00:00', 'B', '18:00:00', 'CompagniaProva', 'NAV-3');
+        RAISE NOTICE 'Trigger 4 - Test Negativo FALLITO (Il trigger non ha bloccato)';
+        test_superato := FALSE;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Trigger 4 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
+    END;
+    -- TEST QUATTRO: nuovo collegamento inizia e termina mentre la barca è utilizzata
+    BEGIN
+        INSERT INTO COLLEGAMENTO VALUES (4, '18', 'A', '17:00:00', 'B', '18:00:00', 'CompagniaProva', 'NAV-3');
+        RAISE NOTICE 'Trigger 4 - Test Negativo FALLITO (Il trigger non ha bloccato)';
+        test_superato := FALSE;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Trigger 4 - Test Negativo SUPERATO (Errore intercettato: %)', SQLERRM;
+    END;
+
+    -- PULIZIA DATI
+    DELETE FROM COLLEGAMENTO WHERE Num = 1 AND Codice = '15' AND NomePartenza = 'A' AND NomeArrivo = 'B' AND CodiceRegistrazione = 'NAV-3';
+    DELETE FROM CITTA WHERE Nome IN ('A', 'B');
+    DELETE FROM PROPRIETA WHERE NomeComp = 'CompagniaProva' AND CodiceRegistrazione = 'NAV-3' AND DataInizio = '2004-03-23';
+    DELETE FROM IMBARCAZIONE WHERE CodiceRegistrazione = 'NAV-3';
+    DELETE FROM COMPAGNIA WHERE Nome = 'CompagniaProva';
+
+    RETURN test_superato;
+END; $$ LANGUAGE plpgsql;
+
 SELECT test_trigger_1_orari();
+
 SELECT test_trigger_3_proprieta();
+
+SELECT test_trigger_4_collegamenti();
